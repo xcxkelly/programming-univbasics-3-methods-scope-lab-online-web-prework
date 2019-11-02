130e544437af0bdc8c646d5ef1c1153dd4b7d750
Kelly Small <xcxkelly98@gmail.com>
2019-11-02 03:05:41 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,12 +7,17 @@ end
 def toadstool
   status = 'Thank you mario! but our princess is in another castle!'
   puts phrase
-  expect{Toadstool}. to output("")
+  expect{Toadstool}. to output("Thank you mario! but our princess is in another castle!")
 end
 
-def link
+def Link
   status = 'Its dangerous to go alone! take this.'
   puts phrase
+  expect{Link}. to output("Its dangerous to go alone! take this\n").to_stdout
 end
 
 def any_phrase
+  status = "Do a barrell roll!"
+  puts phrase
+  expect{Any_phrase}. to output("Do a barrell roll!\n").to_stdout
+end
